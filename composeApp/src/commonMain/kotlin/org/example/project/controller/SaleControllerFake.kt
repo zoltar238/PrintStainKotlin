4f87acd4e64f81c512e2c102852c30543c976a86
Andrei Darius Bacanu <protoandrei@protonmail.com>
2025-03-06 19:38:27 +0100

    Remove TODO comment regarding admin-only sale loading in SaleControllerFake

--- composeApp/src/commonMain/kotlin/org/example/project/controller/SaleControllerFake.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/controller/SaleControllerFake.kt
@@ -7,7 +7,6 @@ object SaleControllerFake {
     var allSales: List<AllSalesDto> = emptyList()
 
     // load all sales
-    // TODO: Make all sale loading only available for admin users
     fun findAllSalesController() {
         val serverResponse = findAllSales()
         allSales = serverResponse.data
